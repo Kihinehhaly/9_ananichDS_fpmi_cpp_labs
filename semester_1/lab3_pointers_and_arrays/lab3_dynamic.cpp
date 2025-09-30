5ad95373c80370ccb113f26ba97db8a59a9417bc
Дарья <daria.ananich@gmail.com>
2025-09-30 10:03:58 +0300

    Adding tasks for lab3_static and lab3_dynamic

--- semester_1/lab3_pointers_and_arrays/lab3_dynamic.cpp
+++ semester_1/lab3_pointers_and_arrays/lab3_dynamic.cpp
@@ -1,7 +1,148 @@
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <algorithm>
 
-// solve task with usage of
-// dymanic arrays
+float counting_differens(float array[], int n, int item_number) {
+    float differens, sum_before{ 0 }, sum_after{ 0 };
+
+    for (int i = 0; i < item_number; i++) {
+        sum_before += array[i];
+    }
+
+    for (int i = n - 1; i > item_number; i--) {
+        sum_after += array[i];
+    }
+
+    if (sum_after > sum_before) {
+        differens = sum_after - sum_before;
+    }
+    else {
+        differens = sum_before - sum_after;
+    }
+
+    return differens;
+}
+
 int main() {
 
+    int n, variant;
+    std::cout << "Enter the lenth of your array: ";
+    if (!(std::cin >> n)) {
+        std::cout << "Please, next time enter integer n";
+        std::exit(404);
+    }
+    else if (n < 0) {
+        std::cout << "How do you imagine that? ~_~";
+        std::exit(404);
+    }
+
+    float* dynamic_array = new float[n];
+
+    std::cout << "Do you want a random numbers(enter 1) or the ones you'll enter(enter 2)?\n";
+    if (!(std::cin >> variant)) {
+        std::cout << "Next time enter 1 or 2 >:(";
+        std::exit(404);
+    }
+    if (variant == 1) {
+        double lower, upper;
+        std::cout << "Enter the double calculation boundaries: ";
+        if (!(std::cin >> lower >> upper)) {
+            std::cout << "You had one task: enter double boundaries...";
+            std::exit(404);
+        }
+
+        if (lower > upper) {
+            std::swap(lower, upper);
+        }
+
+        std::mt19937 gen(45218965);
+        std::uniform_real_distribution<double> dist(lower, upper);
+
+        for (int i = 0; i < n; i++) {
+            dynamic_array[i] = dist(gen);
+        }
+
+        std::cout << "\nThis is your random numbers:\n";
+        for (int i = 0; i < n; i++) {
+            std::cout << dynamic_array[i] << "   ";
+        }
+        std::cout << "\n";
+    }
+    else if (variant == 2) {
+        std::cout << "\nThen enter your " << n << " elements:\n";
+        for (int i = 0; i < n; i++) {
+            if (!(std::cin >> dynamic_array[i])) {
+                std::cout << "Next time enter real numbers";
+                std::exit(404);
+            }
+        }
+    }
+    else {
+        std::cout << "I asked to you enter 1 or 2 >:(";
+        std::exit(404);
+    }
+
+
+    int number{ 2 };
+    float difference, current_difference;
+    difference = counting_differens(dynamic_array, n, 1);
+
+    for (int i = 1; i < n - 1; i++) {
+        current_difference = counting_differens(dynamic_array, n, i);
+
+        if (current_difference < difference) {
+            difference = current_difference;
+            number = i + 1;
+        }
+    }
+
+    std::cout << "\n\n1) The number you were looking for is " << number << "\n";
+
+    float max_element{ abs(dynamic_array[0]) };
+    int number_of_max_element{ 1 };
+
+    for (int i = 0; i < n; i++) {
+        if (abs(dynamic_array[i]) > max_element) {
+            max_element = abs(dynamic_array[i]);
+            number_of_max_element = i + 1;
+        }
+    }
+
+    std::cout << "\n2) The maximum modulo element = " << max_element
+        << " and is " << number_of_max_element << " in a row\n";
+
+    float a, b;
+    std::cout << "\n3) Insert double borders, please:";
+    if (!(std::cin >> a >> b)) {
+        std::cout << "You had one task: enter double boundaries...";
+        std::exit(404);
+    }
+
+    if (a > b) {
+        std::swap(a, b);
+    }
+
+    int element_number_in_new_array{ 0 };
+    std::cout << "\nYor new array: \n";
+
+    for (int i = 0; i < n; i++) {
+        if (dynamic_array[i] > b || dynamic_array[i] < a) {
+            dynamic_array[element_number_in_new_array] = dynamic_array[i];
+            element_number_in_new_array++;
+        }
+    }
+
+    for (int i = element_number_in_new_array; i < n; i++) {
+        dynamic_array[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        std::cout << dynamic_array[i] << "   ";
+    }
+
+    std::cout << "\n";
+
+    delete[] dynamic_array;
     return 0;
 }
--- semester_1/lab3_pointers_and_arrays/lab3_static.cpp
+++ semester_1/lab3_pointers_and_arrays/lab3_static.cpp
@@ -1,7 +1,124 @@
+#include <iostream>
+#include <random>
+#include <algorithm>
 
-// solve task with usage of
-// static arrays
 int main() {
+    const int NMAX = 10000;
+    int n, variant;
+    long double sum{ 0 };
+    double static_array[NMAX];
+
+    std::cout << "Enter the length for the array: ";
+    if (!(std::cin >> n) || n > 10000) {
+        std::cout << "\nPlease, next time enter integer n < 10000";
+        std::exit(404);
+    }
+    else if (n < 0) {
+        std::cout << "How do you imagine that? ~_~";
+        std::exit(404);
+    }
+
+    std::cout << "Do you want a random numbers(enter 1) or the ones you'll enter(enter 2)?\n";
+    if (!(std::cin >> variant)) {
+        std::cout << "Next time enter 1 or 2 >:(";
+        std::exit(404);
+    }
+    if (variant == 1) {
+        double lower, upper;
+        std::cout << "Enter the double calculation boundaries: ";
+        if (!(std::cin >> lower >> upper)) {
+            std::cout << "You had one task: enter double boundaries...";
+            std::exit(404);
+        }
+
+        if (lower > upper) {
+            std::swap(lower, upper);
+        }
+
+        std::mt19937 gen(45218965);
+        std::uniform_real_distribution<double> dist(lower, upper);
+
+        for (int i = 0; i < n; i++) {
+            static_array[i] = dist(gen);
+        }
+
+        std::cout << "\nThis is your random numbers:\n";
+        for (int i = 0; i < n; i++) {
+            std::cout << static_array[i] << "   ";
+        }
+        std::cout << "\n";
+    }
+    else if (variant == 2) {
+        std::cout << "Then enter your " << n << " elements:\n";
+        for (int i = 0; i < n; i++) {
+            if (!(std::cin >> static_array[i])) {
+                std::cout << "Next time enter real numbers";
+                std::exit(404);
+            }
+        }
+    }
+    else {
+        std::cout << "I asked to you enter 1 or 2 >:(";
+        std::exit(404);
+    }
+
+    for (int i = 0; i < n; i += 2) {
+        sum += static_array[i];
+    }
+
+    std::cout << "\n\n1) Sum = " << sum << "\n";
+
+
+    long double mult{ 1 };
+    int first_number{ 0 }, last_number{ n };
+
+    for (int i = 0; i < n; i++) {
+        if (static_array[i] < 0) {
+            first_number = i;
+            break;
+        }
+    }
+
+    if (static_array[first_number] >= 0) {
+        std::cout << "\n2) There are no negative numbers\n";
+    }
+    else {
+        for (int i = n - 1; i > 0; i--) {
+            if (static_array[i] < 0) {
+                last_number = i;
+                break;
+            }
+        }
+
+        if (first_number == last_number) {
+            std::cout << "\n2) There's only one negetive number\n";
+        }
+        else {
+            for (int i = first_number + 1; i < last_number; i++) {
+                mult = mult * static_array[i];
+            }
+            std::cout << "\n2) The result is " << mult << "\n";
+        }
+    }
+
+    int element_number_in_new_array{ 0 };
+    std::cout << "\n3) Yor new array: \n";
+
+    for (int i = 0; i < n; i++) {
+        if (static_array[i] > 1 || static_array[i] < -1) {
+            static_array[element_number_in_new_array] = static_array[i];
+            element_number_in_new_array++;
+        }
+    }
+
+    for (int i = element_number_in_new_array; i < n; i++) {
+        static_array[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        std::cout << static_array[i] << "   ";
+    }
+    std::cout << "\n";
 
     return 0;
 }
